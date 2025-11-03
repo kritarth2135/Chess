13d3517ecd5ac3dc130561f95750085b9cdaa4f5
kritarth2135 <kritarthvarma2135@gmail.com>
2025-11-03 18:32:51 +0530

    Added FEN parser

--- helper.py
+++ helper.py
@@ -157,18 +157,38 @@ def clear_screen() -> None:
 
 """All functions related to FEN parsing."""
 
-def fen_parser(fen_string: str) -> dict[str, Any]:
-    """Takes a FEN string and scrapes all information from it."""
-
+def fen_parser(fen_string: str) -> dict[str, Any] | None:
+    """Takes a FEN string and scrapes all information from it and returns a dictionary of all data."""
+    modified_FEN: str = FEN_to_modified_FEN(fen_string)
+    if not modified_FEN_string_validator(modified_FEN):
+        return None
     
-def FEN_to_modified_FEN(fen_string: str) -> list[str]:
+    FEN_data: dict[str, Any] = {}
+    piece_placement_data, active_color, castling_availability, en_passant_squares, halfmove_count, fullmove_count = modified_FEN.split(" ")
+    
+    FEN_data["piece_placement_data"] = piece_placement_data.split("/")
+    FEN_data["active_color"] = WHITE if active_color == "w" else BLACK
+    FEN_data["castling_availability"] = castling_availability
+    FEN_data["en_passant_squares"] = en_passant_squares
+    FEN_data["halfmove_count"] = int(halfmove_count)
+    FEN_data["fullmove_count"] = int(fullmove_count)
+
+    return FEN_data
+
+def modified_FEN_string_validator(modified_FEN: str) -> bool:
+    '''Takes modified FEN as input and returns True if it is valid.'''
+
+    modified_FEN_regex: str = r"([kqrbnpKQRBNPE]{8}\/){7}[kqrbnpKQRBNPE]{8}\s[wb]\s[01]{4}\s(.*)"
+    return bool(re.fullmatch(modified_FEN_regex, modified_FEN))
+
+def FEN_to_modified_FEN(fen_string: str) -> str:
     """Converts the FEN string into a more usable format by making it fixed length."""
     piece_placement_data, active_color, castling_availability, en_passant_squares, halfmove_count, fullmove_count = fen_string.strip().split(" ")
 
-    modified_piece_placement_data: list[str] = modified_piece_placement(piece_placement_data.split("/"))
+    modified_piece_placement_data: str = "/".join(modified_piece_placement(piece_placement_data.split("/")))
+    updated_castling_availability: str = modified_castling_availability(list(castling_availability))
 
-    modified_castling: str = modified_castling_availability(castling_availability.split(""))
-
+    return " ".join([modified_piece_placement_data, active_color, updated_castling_availability, en_passant_squares, halfmove_count, fullmove_count])
 
 def modified_piece_placement(piece_placement: list[str]) -> list[str]:
     """Converts the part of FEN string which indicates the position of all pieces and replaces the number of spaces with 'E's"""
@@ -185,6 +205,8 @@ def modified_piece_placement(piece_placement: list[str]) -> list[str]:
     return new_piece_placement
 
 def modified_castling_availability(castling_availability: list[str]):
+    """Converts castling availability into a series of 0s and 1s in the sequence shown in valid_castling_sides"""
+
     valid_castling_sides: list[str] = [
         pieces[NOTATION][WHITE][KING],
         pieces[NOTATION][WHITE][QUEEN],
