a283f0e20ed28cadac253bbbc868ab912f13e4dc
kritarth2135 <kritarthvarma2135@gmail.com>
2025-11-04 14:07:17 +0530

    Fixed errors

--- helper.py
+++ helper.py
@@ -6,7 +6,8 @@ import os
 import errors
 from pieces import pieces, NOTATION, KING, QUEEN
 
-"""Constants used."""
+
+# Constants used.
 
 # Length of the square chess grid 
 SIZE: int = 8
@@ -16,7 +17,8 @@ EMPTY: int = -1
 WHITE: int = 0
 BLACK: int = 1
 
-"""Helper Classess."""
+
+# Helper Classess.
 
 class PositionTuple:
     """
@@ -24,12 +26,15 @@ class PositionTuple:
 
     Args:
         position: Tuple of the form (rank, file) where rank and file are the indexes of the grid.
+
+    Attributes:
+        rank: Index of the rank from top of the grid.
+        file: Index of the file from left of the grid.
     """
 
     def __init__(self, position: tuple[int, int]) -> None:
         self.rank: int = position[0]
         self.file: int = position[1]
-        self.position = position
     
     def __add__(self, other: PositionTuple) -> PositionTuple:
         """Return the addition of two PositionTuples."""
@@ -43,38 +48,40 @@ class PositionTuple:
 
         return self.rank == other.rank and self.file == other.file
     
+    def __str__(self) -> str:
+        """Returns the PositionTuple in a readable format"""
+        
+        return f"({self.rank}, {self.file})"
+    
     def on_same_rank_or_file(self, other: PositionTuple | Any) -> bool:
         """Returns true if both PositionTuples refer to the location on same rank OR same file."""
 
         return self.rank == other.rank or self.file == other.file
-    
-    def display(self) -> None:
-        """Print the PositionTuple in a readable format"""
-        
-        print(f"({self.rank}, {self.file})")
-        
+
+
 class MovementTuple:
     """
     Create a MovementTuple.
 
     Args:
         movement: Tuple of the form (initial_position, final_position) where both are of type PositionTuple.
+    
+    Attributes:
+        initial_position: PositionTuple indicating the initial position of the move.
+        final_position: PositionTuple indicating the final position of the move.
     """
 
     def __init__(self, movement: tuple[PositionTuple, PositionTuple]) -> None:
         self.initial_position: PositionTuple = movement[0]
         self.final_position: PositionTuple = movement[1]
-        self.movement = movement
 
-    def display(self) -> None:
+    def __str__(self) -> str:
         """Prints the MovementTuple in a readable format."""
 
-        print("[")
-        self.initial_position.display()
-        self.final_position.display()
-        print("]")
+        return f"[{self.initial_position}, {self.final_position}]"
 
-"""Helper Functions to get position in a particular direction."""
+
+# Helper Functions to get position in a particular direction.
 
 # Constants for direction names
 UP: str = "up"
@@ -88,10 +95,14 @@ DOWN_RIGHT: str = "down_right"
 
 # Constants for calculating directions
 values_to_calculate_relative_direction: dict[str, PositionTuple] = {
-    UP: PositionTuple((-1, 0)), DOWN: PositionTuple((1, 0)),
-    LEFT: PositionTuple((0, -1)), RIGHT: PositionTuple((0, 1)),
-    UP_LEFT: PositionTuple((-1, -1)), UP_RIGHT: PositionTuple((-1, 1)),
-    DOWN_LEFT: PositionTuple((1, -1)), DOWN_RIGHT: PositionTuple((1, 1))
+    UP: PositionTuple((-1, 0)),
+    DOWN: PositionTuple((1, 0)),
+    LEFT: PositionTuple((0, -1)),
+    RIGHT: PositionTuple((0, 1)),
+    UP_LEFT: PositionTuple((-1, -1)),
+    UP_RIGHT: PositionTuple((-1, 1)),
+    DOWN_LEFT: PositionTuple((1, -1)),
+    DOWN_RIGHT: PositionTuple((1, 1))
 }
 
 # Constants for group of directions
@@ -102,12 +113,11 @@ ALL_DIRECTIONS: list[str] = [UP, DOWN, LEFT, RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT
 def get_relative_position(position: PositionTuple, dir: str) -> PositionTuple | None:
     """Takes a PositionTuple and returns a PositionTuple according the direction given or returns None in case of failure."""
 
-    if not position:
-        return None
     relative_position = position + values_to_calculate_relative_direction[dir]
     if is_out_of_bounds(relative_position):
         return None
     return relative_position
+
 
 def is_out_of_bounds(position: PositionTuple) -> bool:
     """Takes a PositionTuple and returns true if it is out of bounds of the grid."""
@@ -117,7 +127,8 @@ def is_out_of_bounds(position: PositionTuple) -> bool:
             return True
     return False
 
-"""Helper functions to validate and convert user input to PositionTuple."""
+
+# Helper functions to validate and convert user input to PositionTuple.
 
 def alg_notation_to_position_tuple(alg_notation: str) -> PositionTuple:
     """Takes algorithmic notation in str and returns a PositionTuple according to it."""
@@ -125,13 +136,15 @@ def alg_notation_to_position_tuple(alg_notation: str) -> PositionTuple:
     position: list[int] = []
     for i in range(len(alg_notation)):
         position.append((ord(alg_notation[i]) - ord("a")) if i == 0 else (SIZE - int(alg_notation[i])))
-    return PositionTuple(tuple(reversed(position)))
+    return PositionTuple(tuple(reversed(position))) # type: ignore
+
 
 def input_str_validator(input_str: str) -> bool:
     """Takes the input string and returns true if it is of the form '<starting_square>,<ending_square>'."""
     
     input_regex = "([a-h][0-8]),([a-h][0-8])"
     return bool(re.fullmatch(input_regex, input_str))
+
 
 def input_str_to_movement_tuple(input_str: str) -> MovementTuple:
     """Takes the input string and returns a MovementTuple according to it."""
@@ -146,7 +159,8 @@ def input_str_to_movement_tuple(input_str: str) -> MovementTuple:
     for string in input_list:
         movement_list.append(alg_notation_to_position_tuple(string))
     
-    return MovementTuple(tuple(movement_list))
+    return MovementTuple(tuple(movement_list)) # type: ignore
+
 
 def clear_screen() -> None:
     """Clears the screen of the terminal."""
@@ -155,7 +169,8 @@ def clear_screen() -> None:
     else:
         os.system("clear")
 
-"""All functions related to FEN parsing."""
+
+# All functions related to FEN parsing.
 
 def fen_parser(fen_string: str) -> dict[str, Any] | None:
     """Takes a FEN string and scrapes all information from it and returns a dictionary of all data."""
@@ -175,11 +190,13 @@ def fen_parser(fen_string: str) -> dict[str, Any] | None:
 
     return FEN_data
 
+
 def modified_FEN_string_validator(modified_FEN: str) -> bool:
     '''Takes modified FEN as input and returns True if it is valid.'''
 
     modified_FEN_regex: str = r"([kqrbnpKQRBNPE]{8}\/){7}[kqrbnpKQRBNPE]{8}\s[wb]\s[01]{4}\s(.*)"
     return bool(re.fullmatch(modified_FEN_regex, modified_FEN))
+
 
 def FEN_to_modified_FEN(fen_string: str) -> str:
     """Converts the FEN string into a more usable format by making it fixed length."""
@@ -189,6 +206,7 @@ def FEN_to_modified_FEN(fen_string: str) -> str:
     updated_castling_availability: str = modified_castling_availability(list(castling_availability))
 
     return " ".join([modified_piece_placement_data, active_color, updated_castling_availability, en_passant_squares, halfmove_count, fullmove_count])
+
 
 def modified_piece_placement(piece_placement: list[str]) -> list[str]:
     """Converts the part of FEN string which indicates the position of all pieces and replaces the number of spaces with 'E's"""
@@ -202,7 +220,9 @@ def modified_piece_placement(piece_placement: list[str]) -> list[str]:
             else:
                 temp_str += rank[i]
         new_piece_placement.append(temp_str)
+    
     return new_piece_placement
+
 
 def modified_castling_availability(castling_availability: list[str]):
     """Converts castling availability into a series of 0s and 1s in the sequence shown in valid_castling_sides"""
--- pieces.py
+++ pieces.py
@@ -2,8 +2,6 @@ from typing import Any
 
 import helper
 
-"""Pieces name and other data."""
-
 # Pieces names
 KING: str = "King"
 QUEEN: str = "Queen"
@@ -20,10 +18,20 @@ MATERIAL: str = "material"
 pieces: dict[str, Any] = {
     SYMBOL: [
         {   # White (0)
-            KING: "♔", QUEEN: "♕", ROOK: "♖", BISHOP: "♗", KNIGHT: "♘", PAWN: "♙"
+            KING: "♔",
+            QUEEN: "♕",
+            ROOK: "♖",
+            BISHOP: "♗",
+            KNIGHT: "♘",
+            PAWN: "♙"
         },
         {   # Black (1)
-            KING: "♚", QUEEN: "♛", ROOK: "♜", BISHOP: "♝", KNIGHT: "♞", PAWN: "♟"
+            KING: "♚",
+            QUEEN: "♛",
+            ROOK: "♜",
+            BISHOP: "♝",
+            KNIGHT: "♞",
+            PAWN: "♟"
         },
         {   # Empty (-1)
             EMPTY: " "   # Unicode U+2001
@@ -31,18 +39,33 @@ pieces: dict[str, Any] = {
     ],
     NOTATION: [
         {   # White (0)
-            KING: "K", QUEEN: "Q", ROOK: "R", BISHOP: "B", KNIGHT: "N", PAWN: "P"
+            KING: "K",
+            QUEEN: "Q",
+            ROOK: "R",
+            BISHOP: "B",
+            KNIGHT: "N",
+            PAWN: "P"
         },
         {   # Black (1)
-            KING: "k", QUEEN: "q", ROOK: "r", BISHOP: "b", KNIGHT: "n", PAWN: "p"
+            KING: "k",
+            QUEEN: "q",
+            ROOK: "r",
+            BISHOP: "b",
+            KNIGHT: "n",
+            PAWN: "p"
         },
         {   # Empty (-1)
             EMPTY: "E"
         }
     ],
     MATERIAL: {
-        KING: float("inf"), QUEEN: 9, ROOK: 5,
-        BISHOP: 3, KNIGHT: 3, PAWN: 1, EMPTY: 0
+        KING: float("inf"),
+        QUEEN: 9,
+        ROOK: 5,
+        BISHOP: 3,
+        KNIGHT: 3,
+        PAWN: 1,
+        EMPTY: 0
     }
 }
 
@@ -53,7 +76,15 @@ class Piece:
     Args:
         color: An integer constant representing the color of the Piece created.
         position: A PositionTuple indicating the position at which the piece to create on grid.
+
+    Attributes:
+        symbol: Icon used to display the piece in the terminal.
+        name: The name of the piece.
+        is_moved: Variable that represents if a piece is moved or not.
+        color: The color of the piece.
+        position: PositionTuple representing the position of the piece on the Board.
     """
+
     def __init__(self, color: int, position: helper.PositionTuple) -> None:
         self.symbol: str
         self.name: str
@@ -69,8 +100,9 @@ class Piece:
             valid_moves: A list of valid moves grouped by direction in case of Queen, Rook, Bishop and
                          a combined list of all valid moves in case of all other pieces
         """
+
         if self.name in [QUEEN, ROOK, BISHOP]:
-            valid_moves: list[list[helper.PositionTuple]] = []
+            valid_moves_per_direction: list[list[helper.PositionTuple]] = []
             temp_list: list[helper.PositionTuple] = []
             valid_directions: list[str] = []
             
@@ -82,22 +114,22 @@ class Piece:
                 valid_directions += helper.DIAGONAL_DIRECTIONS
             
             for direction in valid_directions:
-                position: helper.PositionTuple = self.position
+                position: helper.PositionTuple | None = self.position
                 for _ in range(helper.SIZE):
-                    relative_postion: helper.PositionTuple = helper.get_relative_position(position, direction)
+                    relative_postion: helper.PositionTuple | None = helper.get_relative_position(position, direction)
                     if relative_postion:
                         temp_list.append(relative_postion)
                         position = relative_postion
-                valid_moves.append(temp_list)
+                valid_moves_per_direction.append(temp_list)
                         
-            return valid_moves
+            return valid_moves_per_direction
         
         else:
             valid_moves: list[helper.PositionTuple] = []
 
             if self.name == KING:
                 for direction in helper.ALL_DIRECTIONS:
-                    relative_postion: helper.PositionTuple = helper.get_relative_position(self.position, direction)
+                    relative_postion = helper.get_relative_position(self.position, direction)
                     if relative_postion:
                         valid_moves.append(relative_postion)
 
@@ -108,11 +140,15 @@ class Piece:
                 STRAIGHT_MOVES: int = 2
                 
                 for dir in helper.STRAIGHT_DIRECTIONS:
-                    position: helper.PositionTuple = self.position
-                    for _ in range(STRAIGHT_MOVES):
-                        relative_position: helper.PositionTuple = helper.get_relative_position(position, dir)
+                    position = self.position
+                    for _ in range(STRAIGHT_MOVES): 
+                        if not position:
+                            break
+                        relative_position = helper.get_relative_position(position, dir)
                         position = relative_position
                     for inner_dir in helper.STRAIGHT_DIRECTIONS:
+                        if not position:
+                            break
                         relative_position = helper.get_relative_position(position, inner_dir)
                         if relative_position:
                             if not relative_position.on_same_rank_or_file(self.position):
@@ -123,10 +159,10 @@ class Piece:
             elif self.name == PAWN:
                 valid_no_of_moves: int = 1 if self.is_moved else 2
                 direction: str = helper.DOWN if self.color == helper.BLACK else helper.UP
-                position: helper.PositionTuple = self.position
+                position = self.position
                 
                 for _ in range(valid_no_of_moves):
-                    relative_position: helper.PositionTuple = helper.get_relative_position(position, direction)
+                    relative_position = helper.get_relative_position(position, direction)
                     if relative_position:
                         valid_moves.append(relative_position)
                         position = relative_position
@@ -142,7 +178,7 @@ class Empty(Piece):
     material: int = pieces[MATERIAL][name]
     color: int = helper.EMPTY
     
-    def __init__(self, color, position):
+    def __init__(self, color: int, position: helper.PositionTuple):
         super().__init__(Empty.color, position)
         self.symbol = pieces[SYMBOL][color][Empty.name]
 
@@ -150,7 +186,7 @@ class King(Piece):
     name: str = KING
     material: int = pieces[MATERIAL][name]
 
-    def __init__(self, color, position):
+    def __init__(self, color: int, position: helper.PositionTuple):
         super().__init__(color, position)
         self.symbol = pieces[SYMBOL][color][King.name]
 
@@ -158,7 +194,7 @@ class Queen(Piece):
     name: str = QUEEN
     material: int = pieces[MATERIAL][name]
 
-    def __init__(self, color, position):
+    def __init__(self, color: int, position: helper.PositionTuple):
         super().__init__(color, position)
         self.symbol = pieces[SYMBOL][color][Queen.name]
 
@@ -166,7 +202,7 @@ class Rook(Piece):
     name: str = ROOK
     material: int = pieces[MATERIAL][name]
 
-    def __init__(self, color, position):
+    def __init__(self, color: int, position: helper.PositionTuple):
         super().__init__(color, position)
         self.symbol = pieces[SYMBOL][color][Rook.name]
 
@@ -174,7 +210,7 @@ class Bishop(Piece):
     name: str = BISHOP
     material: int = pieces[MATERIAL][name]
 
-    def __init__(self, color, position):
+    def __init__(self, color: int, position: helper.PositionTuple):
         super().__init__(color, position)
         self.symbol = pieces[SYMBOL][color][Bishop.name]
 
@@ -182,7 +218,7 @@ class Knight(Piece):
     name: str = KNIGHT
     material: int = pieces[MATERIAL][name]
 
-    def __init__(self, color, position):
+    def __init__(self, color: int, position: helper.PositionTuple):
         super().__init__(color, position)
         self.symbol = pieces[SYMBOL][color][Knight.name]
 
@@ -190,6 +226,6 @@ class Pawn(Piece):
     name: str = PAWN
     material: int = pieces[MATERIAL][name]
 
-    def __init__(self, color, position):
+    def __init__(self, color: int, position: helper.PositionTuple):
         super().__init__(color, position)
         self.symbol = pieces[SYMBOL][color][Pawn.name]
