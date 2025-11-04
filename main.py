23e144b1a52c847a58f1842b59db0df1154b08d6
kritarth2135 <kritarthvarma2135@gmail.com>
2025-11-04 18:32:45 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -23,12 +23,17 @@ def main() -> None:
         
         while True:
             try:
-                input_str: str = input("Enter the move to play: ")
+                input_str: str = input("Enter the move to play or 'exit' to quit: ")
+                if input_str.lower() == "exit":
+                    exit()
                 board.move(helper.input_str_to_movement_tuple(input_str))
             except errors.InvalidInput:
+                print("Invalid Input format.")
                 print("Format to enter input: \"<starting_square>,<ending_square>\"")
             except errors.InvalidMove:
                 print("This move is illegal.")
+            except errors.InvalidTurn:
+                print(f"It is {"White" if board.active_color == helper.WHITE else "Black"}'s turn.")
             else:
                 break
 
